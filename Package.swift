cc42787bff6b734c1c426c62e6eb3a43fd845509
tal spektor <talspektor@gmail.com>
2021-07-24 18:40:09 +0300

    register nibs

--- Package.swift
+++ Package.swift
@@ -20,7 +20,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "PopupSPM",
-            dependencies: []),
+            dependencies: [],
+            resources: [
+                .process("View/ButtonsView/ButtonsView.xib"),
+                .process("View/ButtonsView/PopupView.xib")
+            ]),
         .testTarget(
             name: "PopupSPMTests",
             dependencies: ["PopupSPM"]),
