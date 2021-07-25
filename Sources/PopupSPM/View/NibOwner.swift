6b3fa27309a23e40d293d9aee0dc8a7d50488074
Tal <talspektor.gmail.com>
2021-07-25 08:55:56 +0300

    nibable

--- Sources/PopupSPM/View/NibOwner.swift
+++ Sources/PopupSPM/View/NibOwner.swift
@@ -14,6 +14,7 @@ public class Nibable: UIView {
     }
     
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
+        fromNib()
     }
 }
